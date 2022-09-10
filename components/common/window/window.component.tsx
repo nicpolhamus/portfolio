d709c0e007b441c7e6191a9aee81c091a3ef5583
Drew Polhamus <nicolas.polhamus@gmail.com>
2022-09-10 03:02:01 -0400

    - adds close button to menu bar - updates styling

--- components/common/window/window.component.tsx
+++ components/common/window/window.component.tsx
@@ -13,7 +13,7 @@ export interface IWindowProps {
 }
 
 const WindowDiv = styled.div`
-  background: #678199;
+  background: #80868C;
   height: 600px;
   width: 450px;
   border-radius: 6px;
@@ -22,26 +22,33 @@ const WindowDiv = styled.div`
 export function WindowComponent({ defaultPosition }: IWindowProps): ReactElement {
   const basePosition = { x: 0, y: 0 };
   const [translate, setTranslate] = useState(defaultPosition || basePosition);
+  const [isClosed, setIsClosed] = useState(false);
 
   const handleDragMove = (event: any) => {
     setTranslate({
       x: translate.x + event.movementX,
-      y: translate.y + event.movementY, 
+      y: translate.y + event.movementY,
     });
   };
 
-  
+  const handleClose = (event: any) => {
+    setIsClosed(!isClosed);
+  };
+
+
   return (
-    <DragMove onDragMove={handleDragMove}>
-      <div
-        style={{
-          transform: `translateX(${translate.x}px) translateY(${translate.y}px)`,
-        }}>
-        <WindowDiv>
-          <MenuBar />
-          Test
-        </WindowDiv>
-      </div>
-    </DragMove>
+    <>
+      {!isClosed && (<DragMove onDragMove={handleDragMove}>
+        <div
+          style={{
+            transform: `translateX(${translate.x}px) translateY(${translate.y}px)`,
+          }}>
+          <WindowDiv>
+            <MenuBar onClose={handleClose} />
+            Test
+          </WindowDiv>
+        </div>
+      </DragMove>)}
+    </>
   );
 }
--- components/core/menuBar/menuBar.component.tsx
+++ components/core/menuBar/menuBar.component.tsx
@@ -7,28 +7,43 @@ type TMenuItem = {
 };
 
 export interface IMenuBar {
+  onClose: (args: any) => void;
   items?: TMenuItem[];
   help?: string;
   // TODO: add options?
 }
 
 const MenuBarDiv = styled.div`
-  background: rgb(60,75,89);
-  background: -moz-linear-gradient(0deg, rgba(60,75,89,1) 15%, rgba(54,68,81,1) 80%);
-  background: -webkit-linear-gradient(0deg, rgba(60,75,89,1) 15%, rgba(54,68,81,1) 80%);
-  background: linear-gradient(0deg, rgba(60,75,89,1) 15%, rgba(54,68,81,1) 80%);
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#3c4b59",endColorstr="#364451",GradientType=1);
-  border: 2px solid #33404d;
+  background: rgb(71,79,89);
+  background: -moz-linear-gradient(0deg, rgba(71,79,89,1) 0%, rgba(49,58,64,1) 100%);
+  background: -webkit-linear-gradient(0deg, rgba(71,79,89,1) 0%, rgba(49,58,64,1) 100%);
+  background: linear-gradient(0deg, rgba(71,79,89,1) 0%, rgba(49,58,64,1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#474f59",endColorstr="#313a40",GradientType=1);
+  border: 1px solid #313a40;
   border-radius: 6px 6px 0 0;
   height: 30px;
   width: 100%;
 `;
 
-export function MenuBar({ items, help }: IMenuBar): ReactElement {
+const CloseButton = styled.button`
+  float: right;
+  margin: 5px 5px 0 0;
+  width: 5%;
+  border-radius: 100%;
+`;
+
+export function MenuBar({ items, help, onClose }: IMenuBar): ReactElement {
+  const handleClose = (event: any) => {
+    event.preventDefault();
+
+    onClose(event);
+  };
+
   return (
     <>
       <MenuBarDiv>
         test
+        <CloseButton onClick={handleClose}>X</CloseButton>
       </MenuBarDiv>
     </>
   );
